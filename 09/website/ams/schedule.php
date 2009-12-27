b77ad12b148b7d90ab1ab303458eeacdeacb6238
Jason (Linux) <jason@lenovo.localdomain>
2009-12-26 17:42:11 -0800

    Modal popover for location of gym

--- 09/website/ams/schedule.php
+++ 09/website/ams/schedule.php
@@ -8,10 +8,35 @@ $content= array(
         'title' => 'Gym Schedule',
         'text'  => '<img src="imgs/osbourne.jpg" alt="Osbourne Gym" id="imgOsbourne" />
 
-                    <p>Gym nights are held at the "Robert F. Osborne Gym A" which is
-                    located next to Thunderbird Arenas at <a target="_blank" 
-                    href="http://www.maps.ubc.ca/PROD/index_detail.php?show=y,y,y,y,y,y&bldg2Search=n&locat1=430&locat2=">6108
-                    Thunderbird Boulevard</a>.</p>
+                    <script type="text/javascript">
+                        $(document).ready(function(){
+                            $("#open_location").click(function() { 
+                                $("#location").dialog({ 
+                                    bgiframe: true,
+                                    modal: true,
+                                    height: 550,
+                                    width: 500,
+                                    buttons: {
+                                        Close: function() {
+                                            $(this).dialog("close");
+                                        }
+                                    }
+                                });
+                                $("#location").dialog("open"); 
+                            });
+                        });
+                    </script>
+
+                    <div id="location" style="display:none;" title="Robert F. Osbourne Center (Gym A) - Location">
+                        <p>6108 Thunderbird Boulevard<br/>
+                           Vancouver, BC, Canada</p>
+                        <center>
+                        <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=robert+osbourne+gym&amp;sll=49.26011,-123.245994&amp;sspn=0.001057,0.002406&amp;ie=UTF8&amp;hq=robert+osbourne+gym&amp;hnear=&amp;ll=49.260199,-123.244858&amp;spn=0.006295,0.006295&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=robert+osbourne+gym&amp;sll=49.26011,-123.245994&amp;sspn=0.001057,0.002406&amp;ie=UTF8&amp;hq=robert+osbourne+gym&amp;hnear=&amp;ll=49.260199,-123.244858&amp;spn=0.006295,0.006295" style="color:#0000FF;text-align:left">View Larger Map</a></small></center>
+                    </div>
+
+                    <p>Gym nights are held at the "Robert F. Osborne Center - Gym A"
+                    <a id="open_location" href="#">6108 Thunderbird Boulevard</a>,
+                    next to the Thunderbird Sports Center.</p>
 
                     <table border="1" cellpadding="5px">
                     <tr>
